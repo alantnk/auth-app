25d72bdd64f7383d5d40be59e340eba80e8a7d79
alan <alan@tnk.com>
2025-02-07 17:50:41 -0300

    fix: dialog component

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -1,38 +1,43 @@
-'use client';
-import React from 'react';
-import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
-import { useRouter } from 'next/navigation';
-import { LoginForm } from './login-form';
+"use client";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { DialogTitle } from "@radix-ui/react-dialog";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { LoginForm } from "./login-form";
 
 interface LoginButtonProps {
-	children: React.ReactNode;
-	mode?: 'modal' | 'redirect';
-	asChild?: boolean;
+  children: React.ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
 }
 
 export const LoginButton = ({
-	children,
-	mode = 'redirect',
-	asChild,
+  children,
+  mode = "redirect",
+  asChild,
 }: LoginButtonProps) => {
-	const router = useRouter();
-	const handleClick = () => {
-		router.push('/auth/login');
-	};
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/auth/login");
+  };
 
-	if (mode == 'modal') {
-		return (
-			<Dialog>
-				<DialogTrigger asChild={asChild}>{children}</DialogTrigger>
-				<DialogContent className="p-0 w-auto bg-transparent border-none">
-					<LoginForm />
-				</DialogContent>
-			</Dialog>
-		);
-	}
-	return (
-		<span onClick={handleClick} className="cursor-pointer">
-			{children}
-		</span>
-	);
+  if (mode == "modal") {
+    return (
+      <Dialog>
+        <DialogTrigger asChild={asChild}>{children}</DialogTrigger>
+        <DialogTitle></DialogTitle>
+        <DialogContent
+          aria-describedby=""
+          className="p-0 w-auto bg-transparent border-none"
+        >
+          <LoginForm />
+        </DialogContent>
+      </Dialog>
+    );
+  }
+  return (
+    <span onClick={handleClick} className="cursor-pointer">
+      {children}
+    </span>
+  );
 };
